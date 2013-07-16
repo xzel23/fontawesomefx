177fa8f3d1f829682ca3feefb0e89baa949559ce
Lummerland <Lummerland@192.168.0.12>
2013-07-17 00:46:04 +0200

    latest changes

--- src/main/java/de/jensd/fx/fontawesome/AwesomeDude.java
+++ src/main/java/de/jensd/fx/fontawesome/AwesomeDude.java
@@ -1,9 +1,9 @@
 package de.jensd.fx.fontawesome;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBuilder;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.control.LabelBuilder;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.text.Font;
 
 /**
@@ -13,7 +13,7 @@ import javafx.scene.text.Font;
 public class AwesomeDude {
 
     public final static String FONT_AWESOME_TTF_PATH = "/font/fontawesome-webfont.ttf";
-    public final static double DEFAULT_FONT_SIZE = 16.0;
+    public final static String DEFAULT_FONT_SIZE = "1.2em";
     public Font fontAwesome;
     private static AwesomeDude me;
 
@@ -29,42 +29,46 @@ public class AwesomeDude {
 
     public void loadFontAwesome() {
         fontAwesome =
-                Font.loadFont(AwesomeDude.class.getResource(FONT_AWESOME_TTF_PATH).toExternalForm(), DEFAULT_FONT_SIZE);
-
+                Font.loadFont(AwesomeDude.class.getResource(FONT_AWESOME_TTF_PATH).toExternalForm(), 10.0);
     }
 
     public Font getFontAwesome() {
         return fontAwesome;
     }
 
-    public Label createIconLabel(AwesomeIcon icon, double iconSize) {
+    public Label createIconLabel(AwesomeIcon icon, String iconSize) {
         Label label = new Label(icon.toString());
         label.getStyleClass().add("awesome");
-        label.setStyle("-fx-font-family: FontAwesome; -fx-font-size: " + iconSize + "px;");
+        label.setStyle("-fx-font-family: FontAwesome; -fx-font-size: " + iconSize + ";");
         return label;
     }
 
     public Button createIconButton(AwesomeIcon icon) {
-        return createIconButton(icon, "", DEFAULT_FONT_SIZE);
+        return createIconButton(icon, "");
     }
 
     public Button createIconButton(AwesomeIcon icon, String text) {
-        return createIconButton(icon, text, DEFAULT_FONT_SIZE);
+        return createIconButton(icon, text);
     }
 
-    public Button createIconButton(AwesomeIcon icon, double iconSize) {
-        return createIconButton(icon, "", iconSize);
-    }
-
-    public Button createIconButton(AwesomeIcon icon, String text, double iconSize) {
-        Label label = createIconLabel(icon);
+    public Button createIconButton(AwesomeIcon icon, String text, String iconSize, String fontSize, ContentDisplay contentDisplay) {
+        Label label = createIconLabel(icon, iconSize);
         Button button = new Button(text);
+        button.setStyle("-fx-fone-size: 0.8em;");
         button.setGraphic(label);
+        button.setContentDisplay(contentDisplay);
+        return button;
+    }
+
+    public ToggleButton createIconToggleButton(AwesomeIcon icon, String text, String iconSize, ContentDisplay contentDisplay) {
+        Label label = createIconLabel(icon, iconSize);
+        ToggleButton button = new ToggleButton(text);
+        button.setGraphic(label);
+        button.setContentDisplay(contentDisplay);
         return button;
     }
 
     public Label createIconLabel(AwesomeIcon icon) {
-        return createIconLabel(icon, 16);
+        return createIconLabel(icon, DEFAULT_FONT_SIZE);
     }
-
 }
