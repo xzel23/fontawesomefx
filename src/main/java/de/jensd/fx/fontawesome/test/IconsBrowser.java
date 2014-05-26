3e08dee9ede4d9ce20731b20c51b8d7a6d89a1d5
Jerady <jerady@me.com>
2014-05-26 20:49:39 +0200

    prepare for 8.0.8 release

--- src/main/java/de/jensd/fx/fontawesome/test/IconsBrowser.java
+++ src/main/java/de/jensd/fx/fontawesome/test/IconsBrowser.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2013,2014 Jens Deters
+ * http://www.jensd.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 package de.jensd.fx.fontawesome.test;
 
@@ -29,9 +41,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
- * FXML Controller class
  *
- * @author jdeters
+ * @author Jens Deters
  */
 public class IconsBrowser extends VBox {
 
