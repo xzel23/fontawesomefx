62fbd72cb7db7144cdd6d9263d49898293ec436f
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2015-04-17 11:43:54 +0200

    Added GlyphsStack#addAll() methods and updated test classes/fxml to test them

--- src/main/java/de/jensd/fx/glyphs/GlyphsStack.java
+++ src/main/java/de/jensd/fx/glyphs/GlyphsStack.java
@@ -15,6 +15,8 @@ package de.jensd.fx.glyphs;
 
 import javafx.scene.layout.StackPane;
 
+import java.util.Collection;
+
 /**
  *
  * @author Jens Deters
@@ -28,6 +30,46 @@ public class GlyphsStack extends StackPane {
 
     public GlyphsStack add(GlyphIcon icon) {
         getChildren().add(icon);
+        return this;
+    }
+
+    /**
+     * Add all {@code icons} to this {@link GlyphsStack}. If the icons are {@code null} or empty,
+     * nothing is performed.
+     * @param icons The icons to add to this stack.
+     * @return This instance of GlyphsStack.
+     */
+    public GlyphsStack addAll(GlyphIcon ... icons) {
+        if(icons != null && icons.length > 0) {
+              this.getChildren().addAll(icons);
+        }
+        return this;
+    }
+
+    /**
+     * Add all {@code icons} to this {@link GlyphsStack}. If the icons are {@code null} or empty,
+     * nothing is performed.
+     * @param icons The icons to add to this stack.
+     * @return This instance of GlyphsStack.
+     */
+    public GlyphsStack addAll(Collection<? extends GlyphIcon> icons) {
+        if(icons != null && !icons.isEmpty()) {
+            this.getChildren().addAll(icons);
+        }
+        return this;
+    }
+
+    /**
+     * Add all {@code icons} to this {@link GlyphsStack}. If the icons are {@code null} or empty,
+     * nothing is performed.
+     * @param index index at which to insert the first element from the specified collection.
+     * @param icons The icons to add to this stack.
+     * @return This instance of GlyphsStack.
+     */
+    public GlyphsStack addAll(int index, Collection<? extends GlyphIcon> icons) {
+        if(icons != null && !icons.isEmpty()) {
+            this.getChildren().addAll(index, icons);
+        }
         return this;
     }
 
