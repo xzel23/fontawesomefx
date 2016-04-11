98fbbca473b2e0762ce5b349014b26c0bd979395
jens <jens@jenss-mbp-cc.fritz.box>
2016-04-11 21:40:04 +0200

    released as fontawesomefx-8.9 continuing with fontawesomefx-8.10 split into sub artifacts per font

--- src/test/java/de/jensd/fx/glyphs/testapps/App.java
+++ src/test/java/de/jensd/fx/glyphs/testapps/App.java
@@ -27,8 +27,6 @@ import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import de.jensd.fx.glyphs.weathericons.WeatherIconView;
 import de.jensd.fx.glyphs.weathericons.WeatherIcon;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
--- src/test/java/de/jensd/fx/glyphs/testapps/CssParserTest.java
+++ src/test/java/de/jensd/fx/glyphs/testapps/CssParserTest.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.jensd.fx.glyphs.testapps;
 
-import de.jensd.fx.glyphs.GlyphIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import java.util.ArrayList;
 import java.util.List;
 
